f3da2cf341117a7840e6146edd966060d7bfd9ac
Mironov228 <60555031+Mironov228@users.noreply.github.com>
2020-08-14 12:50:12 +0300

    First commit
    First commit

---
+++ build/precache-manifest.87fcecf6dca667cdc2a192de08548799.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b9a3f2886b06e28a65c5de2be2c121",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38fa839f5d8a6aa9ebed",
+    "url": "/static/css/main.290a86a4.chunk.css"
+  },
+  {
+    "revision": "6c4d57a9a91539af546b",
+    "url": "/static/js/2.2163dfe6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.2163dfe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38fa839f5d8a6aa9ebed",
+    "url": "/static/js/main.aa5b739c.chunk.js"
+  },
+  {
+    "revision": "37d42b0d29a215c981b2",
+    "url": "/static/js/runtime-main.b8a49a77.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
